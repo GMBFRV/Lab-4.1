f323ac53adc7056180fe3c1c989512fb7160b846
Gadiev Renat <gadievforever@gmail.com>
2023-12-13 08:58:35 +0200

    [modified] Створено ORM моделі, підключено БД

--- Code/project/app.py
+++ Code/project/app.py
@@ -1,41 +1,46 @@
 import os
-import uuid
 from flask import Flask, request, jsonify
+from flask_migrate import Migrate
 from marshmallow import Schema, fields, validate, ValidationError
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:l4N7328CpBRRlEq9o0tEjvZRkSTWhAY0@dpg-clsl98gcmk4c73cbclt0-a.oregon-postgres.render.com/lab3db_95dp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-# Початкові дані
-users = {
-    'user1': {'user_id': '1', 'user_name': 'John Doe'},
-    'user2': {'user_id': '2', 'user_name': 'Alice Johnson'},
-    'user3': {'user_id': '3', 'user_name': 'Mike Bernard'},
-}
+class User(db.Model):
+    user_id = db.Column(db.String, primary_key=True)
+    user_name = db.Column(db.String, nullable=False)
 
-categories = {
-    '1': {'category_id': '1', 'category_name': 'car'},
-    '2': {'category_id': '2', 'category_name': 'phone'},
-    '3': {'category_id': '3', 'category_name': 'watch'},
-    '4': {'category_id': '4', 'category_name': 'Watches', 'visibility': 'public', 'owner_id': '2'},
-    '5': {'category_id': '5', 'category_name': 'House', 'visibility': 'public', 'owner_id': '1'},
-    '6': {'category_id': '6', 'category_name': 'Healthcare', 'visibility': 'private', 'owner_id': '3'},
-}
 
-records = {
-    'Music': {'record_id': '1', 'user_id': '1', 'category_id': '1', 'creation_data': '23-11-23', 'cost': '1000$'},
-    'Games': {'record_id': '2', 'user_id': '2', 'category_id': '2', 'creation_data': '24-11-23', 'cost': '2000$'},
-    'Food': {'record_id': '3', 'user_id': '3', 'category_id': '3', 'creation_data': '23-11-23', 'cost': '3000$'},
-}
+class Category(db.Model):
+    category_id = db.Column(db.String, primary_key=True)
+    category_name = db.Column(db.String, nullable=False)
+    visibility = db.Column(db.String)
+    owner_id = db.Column(db.String)
+
+
+class Record(db.Model):
+    id = db.Column(db.String, primary_key=True)
+    user_id = db.Column(db.String, db.ForeignKey('user.user_id'), nullable=False)
+    category_id = db.Column(db.String, db.ForeignKey('category.category_id'), nullable=False)
+    creation_data = db.Column(db.String, nullable=False)
+    cost = db.Column(db.String, nullable=False)
+
 
 class UserSchema(Schema):
     user_name = fields.String(required=True)
     user_id = fields.String(required=True)
+
 
 class CategorySchema(Schema):
     category_id = fields.String(required=True)
     category_name = fields.String(required=True, validate=validate.Length(min=1))
     visibility = fields.String(validate=validate.OneOf(['public', 'private']))
     owner_id = fields.String()
+
 
 class RecordSchema(Schema):
     record_id = fields.String(required=True)
@@ -44,77 +49,103 @@ class RecordSchema(Schema):
     creation_data = fields.String(required=True)
     cost = fields.String(required=True)
 
+
+
 user_schema = UserSchema()
 category_schema = CategorySchema()
 record_schema = RecordSchema()
+
+
 
 @app.route('/')
 def hello_world():
     return 'This is Lab-work #3'
 
-# Вивід списку користувачів
-@app.route('/users', methods=['GET'])
-def get_users():
-    return jsonify(users)
 
-# Вивід конкретного користувача по ID
-@app.route('/user/<user_id>', methods=['GET'])
-def get_user(user_id):
-    user = next((user for user in users.values() if user['user_id'] == user_id), None)
-    if user:
-        return jsonify(user)
-    return jsonify({'error': 'User not found'}), 404
-
-# Створення користувача
+# Пользователи
 @app.route('/user', methods=['POST'])
 def create_user():
     try:
         user_data = user_schema.load(request.get_json())
     except ValidationError as err:
         return jsonify({'error': err.messages}), 400
+    new_user = User(user_id=user_data['user_id'], user_name=user_data['user_name'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'User created!'}), 201
 
-    user_id = user_data.get('user_id')
-    user = {'user_id': user_id, **user_data}
-    users[user_id] = user
-    return jsonify(user_schema.dump(user))
 
-# Видалення користувача
-@app.route('/user/<user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    user = next((user for user in users.values() if user['user_id'] == user_id), None)
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify(user_schema.dump(users, many=True))
+
+
+@app.route('/user/<user_id>', methods=['GET'])
+def get_user(user_id):
+    user = User.query.get(user_id)
     if user:
-        deleted_user = users.pop(user_id)
-        return jsonify({'message': 'User deleted successfully', 'user': deleted_user})
+        return jsonify(user_schema.dump(user))
     return jsonify({'error': 'User not found'}), 404
 
-# Вивід списку категорій
-@app.route('/category', methods=['GET'])
-def get_categories():
-    return jsonify(categories)
 
-# Створення категорії
+@app.route('/user/<user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = User.query.get(user_id)
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({'message': 'User deleted successfully'})
+    return jsonify({'error': 'User not found'}), 404
+
+
+# Категории
 @app.route('/category', methods=['POST'])
 def create_category():
     try:
         category_data = category_schema.load(request.get_json())
+        new_category = Category(category_id=category_data['category_id'],
+                                category_name=category_data['category_name'],
+                                visibility=category_data.get('visibility'),
+                                owner_id=category_data.get('owner_id'))
+        db.session.add(new_category)
+        db.session.commit()
+
+        return jsonify(category_schema.dump(new_category))
     except ValidationError as err:
         return jsonify({'error': err.messages}), 400
 
-    category_id = category_data.get('category_id')
-    category = {'category_id': category_id, **category_data}
-    categories[category_id] = category
-    return jsonify(category_schema.dump(category))
 
-# Видалення категорії по ID
-@app.route('/category/<category_id>', methods=['DELETE'])
-def delete_category(category_id):
-    category = next((category for category in categories.values() if category['category_id'] == category_id), None)
+@app.route('/categories', methods=['GET'])
+def get_categories():
+    try:
+        categories = Category.query.all()
+        app.logger.debug("Categories retrieved from the database: %s", categories)
+        return jsonify(category_schema.dump(categories, many=True))
+    except Exception as e:
+        app.logger.exception("An error occurred while getting categories: %s", e)
+        return jsonify({'error': 'Internal Server Error'}), 500
+
+
+@app.route('/category/<category_id>', methods=['GET'])
+def get_category(category_id):
+    category = Category.query.get(category_id)
     if category:
-        deleted_category = categories.pop(category_id)
-        return jsonify({'message': 'Category deleted successfully', 'category': deleted_category})
+        return jsonify(category_schema.dump(category))
     return jsonify({'error': 'Category not found'}), 404
 
-# Створення запису
+
+@app.route('/category/<category_id>', methods=['DELETE'])
+def delete_category(category_id):
+    category = Category.query.get(category_id)
+    if category:
+        db.session.delete(category)
+        db.session.commit()
+        return jsonify({'message': 'Category deleted successfully'})
+    return jsonify({'error': 'Category not found'}), 404
+
+
+# Записи
 @app.route('/record', methods=['POST'])
 def create_record():
     try:
@@ -123,43 +154,54 @@ def create_record():
         return jsonify({'error': err.messages}), 400
 
     category_id = record_data['category_id']
-    category = categories.get(category_id)
+    category = Category.query.get(category_id)
     if category is None:
         return jsonify({'error': 'Category not found'}), 404
 
-    visibility = category.get('visibility', 'public')
-    # Перевірка видимості категорії
+    visibility = category.visibility
     if visibility == 'private':
         user_id = record_data['user_id']
-        if user_id != category.get('owner_id'):
+        if user_id != category.owner_id:
             return jsonify({'error': 'You are not allowed to use this category'}), 403
 
-    record_id = record_data.get('record_id')
-    record = {'record_id': record_id, **record_data}
-    records[record_id] = record
-    return jsonify(record_schema.dump(record))
+    new_record = Record(record_id=record_data['record_id'],
+                        user_id=record_data['user_id'],
+                        category_id=record_data['category_id'],
+                        creation_data=record_data['creation_data'],
+                        cost=record_data['cost'])
 
-# Вивід списку записів по певному користувачу та/або категорії
-@app.route('/record', methods=['GET'])
+    db.session.add(new_record)
+    db.session.commit()
+
+    return jsonify(record_schema.dump(new_record))
+
+
+@app.route('/records', methods=['GET'])
 def get_records():
     user_id = request.args.get('user_id')
     category_id = request.args.get('category_id')
     if not user_id and not category_id:
         return jsonify({'error': 'At least one of user_id or category_id is required'}), 400
 
-    filtered_records = [record for record in records.values() if
-                        (not user_id or record['user_id'] == user_id) and
-                        (not category_id or record['category_id'] == category_id)]
-    return jsonify(filtered_records)
+    if user_id:
+        records = Record.query.filter_by(user_id=user_id).all()
+    elif category_id:
+        records = Record.query.filter_by(category_id=category_id).all()
+    else:
+        records = Record.query.all()
 
-# Видалення запису
+    return jsonify(record_schema.dump(records, many=True))
+
+
 @app.route('/record/<record_id>', methods=['DELETE'])
 def delete_record(record_id):
-    for record_key, record in records.items():
-        if record['record_id'] == record_id:
-            deleted_record = records.pop(record_key)
-            return jsonify({'message': 'record deleted successfully', 'deleted record': deleted_record})
+    record = Record.query.get(record_id)
+    if record:
+        db.session.delete(record)
+        db.session.commit()
+        return jsonify({'message': 'Record deleted successfully'})
     return jsonify({'error': 'Record not found'}), 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5001)))
+    db.create_all()
